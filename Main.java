9012cdcccb6175006eb816b7fefdca35bfd5a67b
Chad Niedert <chad.niedert@icloud.com>
2021-12-02 03:08:32 +0000

    successfully created countsOf1List

--- Main.java
+++ Main.java
@@ -4,18 +4,18 @@ public class Main
 {
   public static void main(String[] args) 
 	{
-		
-    ArrayList<String> allBinList = new ArrayList<String>();
+		ArrayList<String> first20List = new ArrayList<String>();
+    //ArrayList<String> allBinList = new ArrayList<String>();
 
-		int rowsPlusCols = 4;
 		int count = 0;
 
-		for (int i = 0; i < 16; i++)
+		// length 20 (not 40)
+		for (int i = 0; i < 1048576; i++)
 		{
 			String numString = Integer.toBinaryString(i);
-			if (numString.length() < 4)
+			if (numString.length() < 20)
 			{
-				int numZeros = 4 - numString.length();
+				int numZeros = 20 - numString.length();
 				String zeroString = "";
 				for (int j = 0; j < numZeros; j++)
 				{
@@ -23,9 +23,36 @@ public class Main
 				}
 				numString = zeroString + numString;
 			}
-			allBinList.add(numString);
+			first20List.add(numString);
 		}
 
+		ArrayList<Integer> countsOf1List = new ArrayList<Integer>(21);
+
+		// initializes countsOf1List with 21 (0-20) zeros
+		for (int i = 0; i < 21; i++)
+		{
+			countsOf1List.add(0);
+		}
+
+
+		for (String a : first20List)
+		{
+			int count1 = 0;
+			for (int i = 0; i < a.length(); i++)
+			{
+				if (a.substring(i, i + 1).equals("1"))
+				{
+					count1++;
+				}	
+			}
+			int incrNum = countsOf1List.get(count1) + 1;
+			countsOf1List.set(count1, incrNum);
+			//System.out.println("Index: " + count1 + " increased and is now " + incrNum);
+			System.out.println("Completed for a = " + a);
+		}
+
+		System.out.println(countsOf1List);
+/*
 		for (String x : allBinList)
 		{
 			int count0 = 0;
@@ -47,16 +74,9 @@ public class Main
 			{
 				count++;
 			}
-		}
-		
-		/* prints each item in allBinList
-		for (String x : allBinList)
-		{
-			System.out.println(x);
-		}
-		*/
+		} */
 
-		System.out.println("# of lattice paths >>> " + count);
+		// System.out.println("# of lattice paths >>> " + count);
 
 	}
 }
